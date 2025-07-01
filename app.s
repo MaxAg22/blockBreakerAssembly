6178d78b513086d0a6ed2457d474331def9c9e0f
Maximo <maximo.aguero@mi.unc.edu.ar>
2025-07-01 19:24:05 -0300

    feat(app): add movement logic to main rectangle

--- app.s
+++ app.s
@@ -1,15 +1,42 @@
-	.include "constants.s"
-	.include "rectangle.s"
+.include "constants.s"
+.include "rectangle.s"
 
-	.globl main
+.globl main
 
 main:
-	// x0 contiene la direccion base del framebuffer
+    // x0 contiene la direccion base del framebuffer
 	mov x20, x0 // Guarda la dirección base del framebuffer en x20
 
 	mov x9, GPIO_BASE
 	str wzr, [x9, GPIO_GPFSEL0]			// Setea gpios 0 - 9 como lectura
-	
+
+    // Configurar posición inicial para el rectangulo
+
+    // Tamaño del rectángulo (en pixeles)
+    mov  x23, 160               // Ancho (width)
+    mov  x24, 20                // Alto (height)	
+
+	// Calcular punto de inicio del rectángulo (centrado)
+    mov x6, SCREEN_WIDTH
+    mov x7, SCREEN_HEIGHT
+
+	// Posición inical para el ancho: x = (SCREEN_WIDTH  - width)  / 2
+    sub x21, x6, x23
+    lsr x21, x21, #1
+	// Posición inical para la altura: y = (SCREEN_HEIGHT - height)
+    sub x22, x7, x24
+
+    // Dibujar primer rectangulo (única vez)
+    mov x1, x23         // Ancho
+    mov x2, x24         // Altura
+    mov x3, x21         // x-coord 
+    mov x4, x22         // y-coord
+    movz x5, 0xFF, lsl 16 // Color
+    movk x5, 0xFFFF, lsl 0
+    bl draw_rectangle
+
+    mov x25, 0          // Offset del rectanglujo inicial
+
 InfLoop:
 
 	ldr w10, [x9, GPIO_GPLEV0]			// Lee el estado de los GPIO 0 - 31
@@ -19,44 +46,76 @@ InfLoop:
 	// - Al hacer AND revela el estado del bit 2
 	// - Al hacer OR "setea" el bit 2 en 1
 	// - Al hacer AND con el complemento "limpia" el bit 2 (setea el bit 2 en 0)
-	and w11, w10, GPIO_W
+	and w11, w10, GPIO_D
+    and w12, w10, GPIO_A
 
-    //=======================================================================
-    //          DIBUJAR RECTANGULO PRINCIPAL    
-    //=======================================================================
+    // Si W11 es distinto de 0 entonces GPIO_1 (W) fue presionada 
+    cbnz w11, draw_and_move_right
+    cbnz w12, draw_and_move_left
 
-	// Tamaño del rectángulo (en pixeles)
-	mov  x1, 160               // Ancho (width)
-    mov  x2, 20                // Alto (height)	
+    b InfLoop
 
-	// Calcular punto de inicio del rectángulo (centrado)
-    mov x6, SCREEN_WIDTH
-	mov x7, SCREEN_HEIGHT
+draw_and_move_right:
 
-	// Posición inical para el ancho: x = (SCREEN_WIDTH  - width)  / 2
-	sub x3, x6, x1
-	lsr x3, x3, #1
-	// Posición inical para la altura: y = (SCREEN_HEIGHT - height)
-	sub x4, x7, x2
+    add x6, x23, x21 // offset actual + tamaño
+    cmp x6, SCREEN_WIDTH
+    b.eq InfLoop
 
-	// si w11 es 0 entonces el GPIO 1 estaba liberado
-	// de lo contrario será distinto de 0, (en este caso particular 2)
-	// significando que el GPIO 1 fue presionado
-	cbnz w11, draw_red
+    // Borrar el rectangulo anterior
+    // Solo cambia el color, utiliza las ultimas coordenadas.
+    movz x5, 0x00, lsl 16
+    movk x5, 0x0000, lsl 0
+    bl draw_rectangle
 
-draw_white:
-	movz x5, 0xFF, lsl 16      
-	movk x5, 0xFFFF, lsl 0
-	bl draw_rectangle
-	mov x20, x0
-	b InfLoop
+    // Incrementar el offset
+    add x25, x25, 1
 
-draw_red:
-	movz x5, 0xFF, lsl 16      
-	movk x5, 0x0000, lsl 0
-	bl draw_rectangle
-	mov x20, x0
-	b InfLoop
+    // Incrementar x-coord
+    add x21, x21, 1
 
+    // Nueva posición
+    mov x1, x23         // Ancho
+    mov x2, x24         // Altura
+    mov x3, x21         // x-coord 
+    mov x4, x22         // y-coord
+    movz x5, 0xFF, lsl 16 // Color
+    movk x5, 0xFFFF, lsl 0
+    bl draw_rectangle
+    mov x19, 0x500000
+    bl delay_loop
 
+draw_and_move_left:
 
+    // Si el offset actual es 0, no dibujamos más
+    cmp x21, #0
+    b.le InfLoop
+
+    // Borrar el rectangulo anterior
+    // Solo cambia el color, utiliza las ultimas coordenadas.
+    movz x5, 0x00, lsl 16
+    movk x5, 0x0000, lsl 0
+    bl draw_rectangle
+
+    // decrementar el offset
+    sub x25, x25, 1
+
+    // decrementar x-coord
+    sub x21, x21, 1
+
+    // Nueva posición
+    mov x1, x23         // Ancho
+    mov x2, x24         // Altura
+    mov x3, x21         // x-coord 
+    mov x4, x22         // y-coord
+    movz x5, 0xFF, lsl 16 // Color
+    movk x5, 0xFFFF, lsl 0
+    bl draw_rectangle
+    mov x19, 0x500000
+    bl delay_loop
+
+// Corto delay para hacer el movimiento visible
+delay_loop:
+    sub x19, x19, #1
+    cbnz x19, delay_loop
+
+    b InfLoop
--- rectangle.s
+++ rectangle.s
@@ -5,31 +5,51 @@ RECTANGLE_S:
     .include "constants.s"
 
 draw_rectangle:
+    sub sp, sp, #96       
+    stp x1, x2, [sp, #0]
+    stp x3, x4, [sp, #16]
+    stp x5, x6, [sp, #32]
+    stp x7, x8, [sp, #48]
+    str x9,     [sp, #64]
+    str x10,    [sp, #72]
+    str x11,    [sp, #80]
 
     // Cargar SCREEN_WIDTH y calcular stride
-    mov  x6, SCREEN_WIDTH      
-    sub  x7, x6, x1            // SCREEN_WIDTH - width
-    lsl  x7, x7, #2            // Stride en bytes
+    mov x6, SCREEN_WIDTH
+    lsl x7, x6, #2          // SCREEN_WIDTH * 4
+    lsl x8, x1, #2          // width * 4
+    sub x7, x7, x8          // stride = total_bytes_fila - bytes_dibujados
+
 
     // Calcular dirección inicial del framebuffer
  	// Dirección_inicio = Dirección_base + 4 * [x + (y * SCREEN_WIDTH)]
     mul  x8, x4, x6            // y * SCREEN_WIDTH
     add  x8, x8, x3            // + x
     lsl  x8, x8, #2            // * 4 bytes por pixel
-    add  x20, x20, x8            // x0 = framebuffer + offset
+    add  x9, x0, x8            // x9 = framebuffer + offset
 
+    mov x10, x2
 // Loop de dibujo
 draw_rect:
-    mov  x12, x1                	// Contador x
+    mov  x11, x1                	// Contador x
 draw_row:
-    str  w5, [x20]				// Colorear el píxel N
-    add  x20, x20, 4				// Siguiente píxel
-    subs x12, x12, 1				// Decrementar contador X
+    str  w5, [x9]				// Colorear el píxel N
+    add  x9, x9, 4				// Siguiente píxel
+    subs x11, x11, 1				// Decrementar contador X
     b.ne draw_row				// Si no terminó la fila, salto
 
-    add  x20, x20, x7            	// Salto a siguiente fila
-    subs x2, x2, 1             	// Altura--
+    add  x9, x9, x7            	// Salto a siguiente fila
+    subs x10, x10, 1             	// Altura--
     b.ne draw_rect			   	// Si no es la última fila, salto
+
+    ldp x1, x2, [sp, #0]
+    ldp x3, x4, [sp, #16]
+    ldp x5, x6, [sp, #32]
+    ldp x7, x8, [sp, #48]
+    ldr x9,     [sp, #64]
+    ldr x10,    [sp, #72]
+    ldr x11,    [sp, #80]
+    add sp, sp, #96
 
     ret
 
